b104c8823e2f02d9d4a4fcfacb4dd5f15caade5d
Kyle Dituro <kmd0612@gmail.com>
2020-04-23 00:55:42 -0400

    Implemented most of Kyle's code. DO NOT RUN!!! WILL MURDER CPU

--- src/homework4_santander_template.py
+++ src/homework4_santander_template.py
@@ -2,28 +2,36 @@ import sklearn.svm
 import sklearn.metrics
 import numpy as np
 import pandas
+import math as m
 
 # Load data
 d = pandas.read_csv('train.csv')
 y = np.array(d.target)  # Labels
 X = np.array(d.iloc[:,2:])  # Features
 
-# Split into train/test folds
-# TODO
+
+# Split X, y into train/test folds (Xtr, ytr/Xte, yte)
+p = np.random.permutation(X.shape[0])
+Xp, yp = X[p], y[p]
+
+Xtr, ytr = Xp[0:m.floor((X.shape[0])/2)], yp[0:m.floor((X.shape[0])/2)]
+Xte, yte = Xp[m.floor((X.shape[0])/2):], yp[m.floor((X.shape[0])/2):]
+
+
 
 # Linear SVM
-# TODO
+svmL = sklearn.svm.SVC(kernel = 'linear', C = 1e15)
+svmL.fit(X = Xtr, y = ytr)
+yhatL = svmL.decision_function(X = Xtr) # Linear kernel
 
 # Non-linear SVM (polynomial kernel)
-# TODO
-
-# Apply the SVMs to the test set
-#yhat1 = ...  # Linear kernel
-#yhat2 = ...  # Non-linear kernel
+svmP = sklearn.svm.SVC(C = 1e15, kernel = 'poly', degree = 3)
+svmP.fit(X = Xtr, y = ytr)
+yhatP = svmP.decision_function(X = Xte) # Non-linear kernel
 
 # Compute AUC
-#auc1 = ...
-#auc2 = ...
+aucL = sklearn.metrics.auc(yhatL, yte)
+aucP = sklearn.metrics.auc(yhatP, yte)
 
 print(auc1)
 print(auc2)
